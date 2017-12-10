7936d9140b8cc81e1c60617503c9ef133a956886
Yu Zhipeng <yzp1011@qq.com>
2017-12-09 17:44:00 -0800

    add del

--- index.py
+++ index.py
@@ -5,6 +5,9 @@ class VideoCam(object):
 	def __init__(self):
 		self.video = cv2.VideoCapture(0)
 
+	def __del__(self):
+		self.video.release()
+
 	def get_frame(self):
 		success,image = self.video.read()
 		ret,jpeg = cv2.imencode('.jpg',image)
