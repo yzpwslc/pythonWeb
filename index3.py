710d7ebf01c3192966bda5c160b29222a5065ca2
yzp1011 <yzp1011@qq.com>
2017-12-17 21:23:59 +0800

    win10 tro

--- index3.py
+++ index3.py
@@ -9,15 +9,14 @@ import time
 from tornado.options import define,options
 define("port",default=8000,help="run on the given port",type=int)
 
-class IndexHandler(tornado.web.Application):
-	def __init__(self):
-		self.video = cv2.VideoCapture(0)
-	def __del__(self):
-		self.video.release()
+class IndexHandler(tornado.web.RequestHandler):
+	def get(self):
+		greeting = self.get_argument('greeting','Hello')
+		self.write(greeting + ', friendly user2!')
 		
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
-	app = IndexHandler()
+	app = tornado.web.Application(handlers=[(r"/",IndexHandler)],debug=True)
 	http_server = tornado.httpserver.HTTPServer(app)
 	http_server.listen(options.port)
 	tornado.ioloop.IOLoop.instance().start()
