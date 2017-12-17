4a3bb647ebc6ce7c09045f58d5f1362af0bfed48
yzp1011 <yzp1011@qq.com>
2017-12-17 21:10:48 +0800

    win10 tro

--- index3.py
+++ index3.py
@@ -11,9 +11,6 @@ define("port",default=8000,help="run on the given port",type=int)
 
 class IndexHandler(tornado.web.RequestHandler):
 	def __init__(self):
-		self.video = cv2.VideoCapture(0)
-	def __del__(self):
-		self.video.release()
 	def get(self):
 		greeting = self.get_argument('greeting','Hello')
 		self.write(greeting + ', friendly user2!')
