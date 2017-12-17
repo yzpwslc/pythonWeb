99933fb77605990592156c7bcfc6d03c03a986f4
yzp1011 <yzp1011@qq.com>
2017-12-17 20:53:26 +0800

    win10 tro

--- index2.py
+++ index2.py
@@ -28,17 +28,16 @@ class camHandler(tornado.web.RequestHandler):
 class IndexHandler(tornado.web.RequestHandler):
 	def __init__(self):
 		self.video = cv2.VideoCapture(0)
-		handlers=[(r"/",IndexHandler),(r"/cam",camHandler)]
 		tornado.web.Application.__init__(self, handlers, debug = True ,cookie_secret = "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o")
 	def __del__(self):
 		self.video.release()
 	def get(self):
 		greeting = self.get_argument('greeting','Hello')
-		self.write(greeting + ', friendly user!')
+		self.write(greeting + ', friendly user2!')
 		
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
-	app = tornado.web.Application()
+	app = tornado.web.Application(handlers=[(r"/",IndexHandler),(r"/cam",camHandler)])
 	http_server = tornado.httpserver.HTTPServer(app)
 	http_server.listen(options.port)
 	tornado.ioloop.IOLoop.instance().start()
