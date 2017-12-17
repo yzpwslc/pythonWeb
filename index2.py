f59fe4f55dfbc3b8bf71d2650e6fd7438e7cc6f8
yzp1011 <yzp1011@qq.com>
2017-12-17 11:44:37 +0000

    raspi c

--- index2.py
+++ index2.py
@@ -4,16 +4,16 @@ import tornado.options
 import tornado.web
 
 from tornado.options import define,options
-define("port",default=8080,help="run on the given port",type=int)
+define("port",default=80,help="run on the given port",type=int)
 
-class IndexHander(tornado.web.RequesttHander):
+class IndexHandler(tornado.web.RequestHandler):
 	def get(self):
 		greeting = self.get_argument('greeting','Hello')
 		self.write(greeting + ', friendly user!')
 		
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
-	app = tornado.web.Application(handers=[(r"/",IndexHander)])
+	app = tornado.web.Application(handlers=[(r"/",IndexHandler)])
 	http_server = tornado.httpserver.HTTPServer(app)
 	http_server.listen(options.port)
-	tornado.ioloop.IOloop.instance().start()
+	tornado.ioloop.IOLoop.instance().start()
