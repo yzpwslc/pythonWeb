10263588cac6c925bb8704c1abf6fb8ecebadfd2
yzp1011 <yzp1011@qq.com>
2018-01-27 11:06:21 +0800

    win10

--- index2.py
+++ index2.py
@@ -3,26 +3,12 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
-import cv2
 import time
 
 from tornado.options import define,options
-define("port",default=80,help="run on the given port",type=int)
+define("port",default=8000,help="run on the given port",type=int)
 
-# class camHandler(tornado.web.RequestHandler):
-	# def get(self):
-		# ret,img = self.IndexHandler.video.read()
-		# if ret:
-			# self.set_header("Content-Type", "image/jpeg")
-			# self.set_header("Refresh", "1")
-			# self.set_header("content-transfer-encoding", "binary")
-			# r,i = cv2.imencode('.jpg',img)
-			# if r:
-				# self.write(bytes(i.data))
-			# else:
-				# self.write('Sorry,encode fail!')
-		# else:
-			# self.write('Sorry,get cam data failed!')	
+
 		
 
 class IndexHandler(tornado.web.RequestHandler):
@@ -33,10 +19,13 @@ class IndexHandler(tornado.web.RequestHandler):
 	def get(self):
 		greeting = self.get_argument('greeting','Hello')
 		self.write(greeting + ', friendly user2!')
+class controlHander(tornado.web.RequestHandler):
+	def get(self):
+		self.write('control');
 		
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
-	app = tornado.web.Application(handlers=[(r"/",IndexHandler)],debug=True)
+	app = tornado.web.Application(handlers=[(r"/",IndexHandler),(r"/led1",controlHander)],debug=True)
 	http_server = tornado.httpserver.HTTPServer(app)
 	http_server.listen(options.port)
 	tornado.ioloop.IOLoop.instance().start()
