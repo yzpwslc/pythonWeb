cabdbf163e272fa3fbe4ef905099b9a1ab7c28ad
yzp1011 <yzp1011@qq.com>
2018-01-27 12:07:20 +0800

    win10

--- index.py
+++ index.py
@@ -6,7 +6,6 @@ class MyResponse(Response):
     def __init__(self, response=None, **kwargs):
         kwargs['headers'] = ''
         headers = kwargs.get('headers')
-        # 跨域控制 
         origin = ('Access-Control-Allow-Origin', '*')
         methods = ('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, DELETE, PUT')
         if headers:
